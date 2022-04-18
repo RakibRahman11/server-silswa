63b80dcc470ed663279b6aa501db7c784cf1d845
RakibRahman11 <rahmanrakib207@gmail.com>
2022-04-19 03:24:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpttu.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("silswa_portal");
+        const counselingCollection = database.collection("counseling");
+
+        // POST counseling appointment
+        app.post('/counseling', async (req, res) => {
+            const appointmentInfo = req.body
+            const result = await counselingCollection.insertOne(appointmentInfo)
+            res.json(result)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello silswa!')
+})
+
+app.listen(port, () => {
+    console.log(`working port ${port}`)
+})
